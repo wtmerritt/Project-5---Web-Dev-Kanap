d3309b29ba73d234cb5b4ea6697359462cec1717
William Merritt <wtmerritt@gmail.com>
2024-08-23 19:31:13 -0400

    Completed Milestone 8

--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -15,6 +15,7 @@ let count = 0;
 
 // Loop thru local storage
 cart.forEach((cartData) => {
+  // console.log(cartData);
   productID = cartData.id;
   const productUrl = `http://localhost:3000/api/products/${productID}`;
 
@@ -24,62 +25,44 @@ cart.forEach((cartData) => {
       return data.json();
     })
     .then((product) => {
-      productColor = cartData.color;
-      productName = product.name;
+      const articleElement = document.createElement("article");
+      articleElement.classList.add("cart__item");
+      articleElement.innerHTML = `<div class="cart__item__img">      
+                  <img src="${product.imageUrl}" alt="${product.altTxt}">
+                </div>
+                <div class="cart__item__content">
+                  <div class="cart__item__content__description">
+                    <h2>${product.name}</h2>
+                    <p>${cartData.color}</p>
+                    <p>€${product.price}</p>
+                  </div>
+                  <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                      <p>Quantity : </p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${cartData.quantity}>
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                      <p class="deleteItem">Delete</p>
+                    </div>
+                  </div>
+                </div>`;
+
       price = parseFloat(product.price);
       // console.log("Price = ", price);
       quantity = parseInt(cartData.quantity);
       // console.log("Quantity = ", quantity);
-      subTotal = parseInt(price * quantity);
+      subTotal = parseFloat(price * quantity);
       totalPrice = subTotal + totalPrice;
-      console.log("Sub Total = ", subTotal);
-      console.log("Total Price = ", totalPrice);
+      // console.log("Sub Total = ", subTotal);
+      // console.log("Total Price = ", totalPrice);
       count += 1;
 
-      // Display Image
-      const cartImage = document.createElement("div");
-      cartImage.className = "cart__item__img";
-
-      cartImage.innerHTML = `
-        <article>          
-          <img src="${product.imageUrl}" alt="${product.altTxt}" width=\"200px\" height=\"200px\">
-        </article>
-          `;
-      document.getElementById("cart__items").appendChild(cartImage);
-
-      // Display Cart Info
-      const cartElement = document.createElement("div");
-      cartElement.className = "cart__item__content__description";
-      cartElement.style.marginLeft = "650px";
-
-      cartElement.innerHTML = `
-        <article>  
-          <h2>${productName}</h2>
-          <p>${productColor}</p>
-          <p>€${price}</p>
-          <p>Quantity: ${quantity}</p>
-        </article>
-          `;
-
-      document.getElementById("cart__items").appendChild(cartElement);
-
-      // Display Delete Option
-      const cartDelete = document.createElement("div");
-      cartDelete.className = "cart__item__content__settings__delete";
-      cartDelete.style.marginLeft = "650px";
-
-      cartDelete.innerHTML = `
-        <article>          
-          <p>Delete</p>
-        </article>
-          `;
-      document.getElementById("cart__items").appendChild(cartDelete);
+      document.getElementById("cart__items").appendChild(articleElement);
 
       // Display Total Price
       const cartPrice = document.querySelector(".cart__price");
-
       cartPrice.innerHTML = `
-        <article>          
+        <article>
           <p>Total (${count} articles): €${totalPrice}</p>
         </article>
           `;
